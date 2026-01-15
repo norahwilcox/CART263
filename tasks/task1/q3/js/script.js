66b6d4699e55106eef9f8b82570a2cce5464a021
norahwilcox <norah@wilcoxfamily.ca>
2026-01-15 16:59:47 -0500

    Finished tasks 3 and 4

--- tasks/task1/q3/js/script.js
+++ tasks/task1/q3/js/script.js
@@ -1,53 +1,80 @@
 "use strict";
 
-let circle = {
-    x: 200,
+let rect1 = {
+    x: 39,
+    y: 60,
+    s: 30,
+    r: 30, 
+    g: 190,
+    b: 200
+}
+
+let rect2 = {
+    x: 300,
     y: 200,
-    size: 70,
-    fill: 200,
+    s: 70,
+    r: 30, 
+    g: 190,
+    b: 200
 }
     
+let rect3 = {
+    x: 200,
+    y: 150,
+    ySpeed: 1,
+    s: 60,
+    r: 70, 
+    g: 200,
+    b: 100
+}   
+    
 function setup() {
-    console.log("go")
+    console.log("go");
     createCanvas(400, 400);
+ 
 }
 
 function draw() {
     background(0);
+    drawRect1();
+    drawRect2();
+    drawRect3();
 
-    drawRect1
+       if (mouseIsPressed) {
+        rect1.x = rect1.x + 10;
+       }
+    if (keyIsPressed) {
+        if (keyCode === 32)
+            rect2.x = rect2.x - 10;
+    }
+
+    rect3.y += rect3.ySpeed
+
+    if (rect3.y === 400) {
+        rect3.y = 0;
+    }
 }
 
 function drawRect1() { 
     push();
     noStroke();
-    fill(90, 150, 159);
-    rect(39, 60, 30);
+    fill(rect1.r, rect1.g, rect1.b);
+    rect(rect1.x, rect1.y, rect1.s);
     pop();
 }
 
 function drawRect2() { 
     push();
     noStroke();
-    fill(30, 190, 200);
-    rect(300, 200, 70);
+    fill(rect2.r, rect2.g, rect2.b);
+    rect(rect2.x, rect2.y, rect2.s);
     pop();
 }
 
 function drawRect3() { 
     push();
     noStroke();
-    fill(70, 200, 100);
-    rect(39, 60, 60);
+    fill(rect3.r, rect3.g, rect3.b);
+    rect(rect3.x, rect3.y, rect3.s);
     pop();
-}
-
-function moveRect() {
-    if (mousePressed) {
-    
-}
-}
-
-function mousePressed() {
-    
 }
